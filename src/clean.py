694637c91ddae4b86ff1f0981a082673e20651d2
fdrigui <filipi.rigui@outlook.com>
2021-09-30 23:43:21 -0300

    Clean Data - Missing Data

--- src/clean.py
+++ src/clean.py
@@ -7,15 +7,16 @@ Created on Wed Sep 29 22:51:09 2021
 """
 
 import pandas as pd
+import numpy as np
 
 def print_nan_count_by_feature(df: pd.DataFrame):
     '''
-    descroption
+    Imprime a contagem de observações com o valor NaN sumarizado por feature
 
     Parameters
     ----------
     df : pd.DataFrame
-        DESCRIPTION.
+        O DataFrame que se deseja sumarizar.
 
     Returns
     -------
@@ -23,44 +24,74 @@ def print_nan_count_by_feature(df: pd.DataFrame):
     
     Examples
     --------
-    >>> from lazypredict.Supervised import LazyClassifier
-    >>> from sklearn.datasets import load_breast_cancer
-    >>> from sklearn.model_selection import train_test_split
-    >>> data = load_breast_cancer()
-    >>> X = data.data
-    >>> y= data.target
-    >>> X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=.5,random_state =123)
-    >>> clf = LazyClassifier(verbose=0,ignore_warnings=True, custom_metric=None)
-    >>> models,predictions = clf.fit(X_train, X_test, y_train, y_test)
-    >>> models
-
+    >>> import sys
+    >>> import pandas as pd
+    >>> sys.path.insert(1, "../src")
+    >>> from clean import print_nan_count_by_feature
+    >>> df_path = 'https://github.com/fdrigui/covid19_icu_admission_prediction/raw/main/data/raw/Kaggle_Sirio_Libanes_ICU_Prediction.xlsx'
+    >>> df = pd.read_excel(df_path)
+    >>> print_nan_count_by_feature(df)
+    
+    Count - Feature Name
+    0000  - PATIENT_VISIT_IDENTIFIER
+    0000  - AGE_ABOVE65
+    0000  - AGE_PERCENTIL
+    0000  - GENDER
+    0005  - DISEASE GROUPING 1
+    0005  - DISEASE GROUPING 2
+    0005  - DISEASE GROUPING 3
+    0005  - DISEASE GROUPING 4
+    0005  - DISEASE GROUPING 5
+    0005  - DISEASE GROUPING 6
+    0005  - HTN
+    ...
     '''
+    
+    # Imprime o cabeçalho
+    print('Count - Feature Name\n--------------------')
+    
+    # Conta a quantidaded de NaN por feature
     nan_by_col = df.isna().sum()
+    
+    # Itera para cada feature, imprimindo a quantidade e o nome de cada uma
     for i in range(nan_by_col.size):
         label = nan_by_col.index[i]
         nan_count = nan_by_col[i]
-        print(f'{str(nan_count).zfill(4)} - {label}')
-        
-        
-def fill_missing_data(df:pd.DataFrame, method:str)->pd.DataFrame:
+        print(f'{str(nan_count).zfill(4)}  - {label}')
+
+
+def fill_missing_data(df:pd.DataFrame, grouby:[str], method: str = None)->pd.DataFrame:
     '''
-    description
+    Agrupa o Dataframe de entrada pela feature
 
     Parameters
     ----------
     df : pd.DataFrame
-        DESCRIPTION.
-    method : str
-        DESCRIPTION.
+        DataFrame que se deseja aplicar o método de preenchimento
+    grouby : [str]
+        Usado para descrever por qual feature(s) o DataFrame vai ser agrupado
+    method : {‘backfill’, ‘bfill’, ‘pad’, ‘ffill’, None}
+        Método de preenchimento das colunas (Features) do DataFrame
 
     Returns
     -------
     filled_concatenated_dataset : TYPE
-        DESCRIPTION.
+        Retorna dataframe preenchido conforme método de Filtro e agrupamento de entrada
+    
+    Examples
+    --------
+    >>> import sys
+    >>> import pandas as pd
+    >>> sys.path.insert(1, "../src")
+    >>> from clean import print_nan_count_by_feature
+    >>> df_path = 'https://github.com/fdrigui/covid19_icu_admission_prediction/raw/main/data/raw/Kaggle_Sirio_Libanes_ICU_Prediction.xlsx'
+    >>> df = pd.read_excel(df_path)
+    >>> df_filled = fill_missing_data(df, 'PATIENT_VISIT_IDENTIFIER', 'bfill')
+    >>> df_filled.head()
 
     '''
     continuos_feature_columns = df.iloc[:, 13:-2].columns
-    filled_coltinuous_features = df.groupby("PATIENT_VISIT_IDENTIFIER", as_index=False)[continuos_feature_columns].fillna(method=method)
+    filled_coltinuous_features = df.groupby(grouby, as_index=False)[continuos_feature_columns].fillna(method=method)
     categorical_features = df.iloc[:, :13]
     output = df.iloc[:, -2:]
     
@@ -69,23 +100,162 @@ def fill_missing_data(df:pd.DataFrame, method:str)->pd.DataFrame:
     
     return filled_concatenated_dataset
 
-def neighborhood_missing_data(df:pd.DataFrame)->pd.DataFrame:
+
+def neighborhood_missing_data(df:pd.DataFrame, grouby:[str])->pd.DataFrame:
     '''
-    descr.
+    Preenche os valores NaN agrupando por 'PATIENT_VISIT_IDENTIFIER'.
+    Primeiro faz o 'Backward fill', preenchendo para as lacunas de NaN para trás.
+    Depois faz o 'Foward fill', preenchendo as lacunas de NaN para frente.
 
     Parameters
     ----------
     df : pd.DataFrame
+        O DataFrame que se deseja aplicar o método de preenchimento
+
+    Returns
+    -------
+    filled : pd.DataFrame
+        DataFrame com os dados preenchidos
+    
+    Examples
+    --------
+    >>> import sys
+    >>> import pandas as pd
+    >>> sys.path.insert(1, "../src")
+    >>> from clean import print_nan_count_by_feature
+    >>> df_path = 'https://github.com/fdrigui/covid19_icu_admission_prediction/raw/main/data/raw/Kaggle_Sirio_Libanes_ICU_Prediction.xlsx'
+    >>> df = pd.read_excel(df_path)
+    >>> df_without_nan = neighborhood_missing_data(df)
+    >>> df_without_nan.head()
+
+    '''
+    
+    # Faz o preenchimento para trás
+    bfill = fill_missing_data(df, grouby, 'bfill')
+    
+    # Faz o preenchimento para frente
+    filled = fill_missing_data(bfill, grouby, 'ffill')
+    return filled
+
+
+def drop_features_with_same_value_for_all_observations(df:pd.DataFrame, print_dropped_columns:bool= False)-> pd.DataFrame:
+    '''
+    Remove do DataFrame Features que contém um único valor, repetido em todas as observações
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        O DataFrame que se deseja demover as features com mesmo valor
+    print_dropped_columns : bool, optional
+        Imprime as colunas que foram removidas. The default is False.
+
+    Returns
+    -------
+    pd.DataFrame
+        DataFrame sem fratures que repetem os valores em todas as observações
+        
+    Examples
+    --------
+    >>>
+
+    '''
+    unique = df.nunique() == 1
+    if print_dropped_columns:
+        for column in df.columns[unique]:
+            print(column)
+            
+    print(f'Total of dropped columns: {sum(unique)}')
+    
+    return df.loc[:,~unique]
+
+def plot_features_with_same_value_for_all_observations(df:pd.DataFrame)-> pd.DataFrame:
+    '''
+    Impime o nome das features que tem um único valor repetido em todas as observações
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        DataFrame que se deseja avaliar
+
+    Examples
+    --------
+    >>>
+    '''
+    unique = df.nunique() == 1
+
+    return unique         
+
+
+
+def drop_duplicated_features(df:pd.DataFrame, print_dropped_columns:bool= False)-> pd.DataFrame:
+    '''
+    Avalia se duas ou mais features são identicas ( que tem valores iguais para cada observação),
+    e retorna uma única feature, removendo as demais que são duplicadas.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        DataFrame que se deseja avaliar
+    print_dropped_columns : bool, optional
+        Mostra quais colunas foram removidas no processo de comparação de features
+
+    Returns
+    -------
+    pd.DataFrame
+        Retorna o DataFrame limpo, ou seja, com as features identicas removidas.
+
+    '''
+    
+    duplicated_columns = df.T.duplicated()
+    if print_dropped_columns:
+        for column in df.columns[duplicated_columns]:
+            print(column)
+    
+    print(f'Total dropped columns: {sum(duplicated_columns)}')
+    
+    return df.loc[:,~duplicated_columns]
+
+
+def plot_duplicated_features(df:pd.DataFrame)-> pd.DataFrame:
+    '''
+    Imprime o nome de features que são identicas ( que tem valores iguais para cada observação).
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        DataFrame que se deseja avaliar
+
+    Returns
+    -------
+    TYPE
+        DESCRIPTION.
+
+    '''
+    
+    duplicated_columns = df.T.duplicated()
+    
+    return df.loc[:,duplicated_columns]
+
+
+def prepare_window(rows):
+    '''
+    descr
+
+    Parameters
+    ----------
+    rows : TYPE
         DESCRIPTION.
 
     Returns
     -------
-    ffill : TYPE
+    TYPE
         DESCRIPTION.
 
     '''
-    bfill = fill_missing_data(df, 'bfill')
-    ffill = fill_missing_data(bfill, 'ffill')
-    return ffill
+    if(np.any(rows["ICU"])):
+        rows.loc[rows["WINDOW"]=="0-2", "ICU"] = 1
+    return rows.loc[rows["WINDOW"] == "0-2"]
+
+
     
     
